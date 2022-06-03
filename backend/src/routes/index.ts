2ba172d5e9af67376cbd11d5405fb7c7e6b9e306
Geovane Silva <geovanesilva.nar@hotmail.com>
2022-06-03 15:17:27 -0300

    Rotas

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,25 @@
-import express  from "express";
+import express from "express";
+import { authController } from "../controller/AuthController";
+import { ClientController } from "../controller/ClientsController";
+import { UsersController } from "../controller/UsersController";
+import Login from "../validations/login/Login";
+
+
+
 
 const routes = express.Router();
 
+routes.get('/users', UsersController.getUsers);
+routes.post('/users', UsersController.createUser);
+routes.put('/users/:id', UsersController.updateUser);
+routes.delete('/users/:id', UsersController.deleteUser);
 
+routes.get('/clients', ClientController.getClients);
+routes.post('/clients', ClientController.createClient);
+routes.put('/clients/:id', ClientController.updateClient);
+routes.delete('/clients/:id', ClientController.deleteClient);
 
-
+routes.post('/login', Login, authController.login);
 
 
 
