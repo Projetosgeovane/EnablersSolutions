280e863f9b912266b4f788e9abb7cea5f30c003f
Geovane Silva <geovanesilva.nar@hotmail.com>
2022-06-04 12:53:19 -0300

    Add nas routes auth e validação de dados

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,24 +2,29 @@ import express from "express";
 import { authController } from "../controller/AuthController";
 import { ClientController } from "../controller/ClientsController";
 import { UsersController } from "../controller/UsersController";
-import Login from "../validations/login/Login";
+import { Auth } from "../middlewares/Auth";
+import { UsersValidation } from "../validations";
+import { ClientsValidation } from "../validations/client";
+import { login } from "../validations/login/login";
+
+
 
 
 
 
 const routes = express.Router();
 
-routes.get('/users', UsersController.getUsers);
-routes.post('/users', UsersController.createUser);
-routes.put('/users/:id', UsersController.updateUser);
-routes.delete('/users/:id', UsersController.deleteUser);
+routes.get('/users', Auth, UsersController.getUsers);
+routes.post('/users', Auth, UsersValidation.create, UsersController.createUser);
+routes.put('/users/:id', Auth, UsersValidation.update, UsersController.updateUser);
+routes.delete('/users/:id', Auth, UsersValidation.destroy, UsersController.deleteUser);
 
-routes.get('/clients', ClientController.getClients);
-routes.post('/clients', ClientController.createClient);
-routes.put('/clients/:id', ClientController.updateClient);
-routes.delete('/clients/:id', ClientController.deleteClient);
+routes.get('/clients', Auth, ClientController.getClients);
+routes.post('/clients', Auth, ClientsValidation.createClient, ClientController.createClient);
+routes.put('/clients/:id', Auth, ClientsValidation.updateClient, ClientController.updateClient);
+routes.delete('/clients/:id', Auth, ClientsValidation.destroyClient, ClientController.deleteClient);
 
-routes.post('/login', Login, authController.login);
+routes.post('/login', login, authController.login);
 
 
 
