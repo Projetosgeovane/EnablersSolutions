e285f93281af43ec6007c7fd18a97b70faf63d73
Geovane Silva <geovanesilva.nar@hotmail.com>
2022-06-04 13:24:25 -0300

    Services para separar regras do Controller no futuro

--- backend/src/services/UsuarioService.ts
+++ backend/src/services/UsuarioService.ts
@@ -1,5 +1,21 @@
 
+import knex from '../database';
 
-export class PsicologoService {
+
+export class UsuarioService {
+    async cadastrarUsuario(data: any) {
+     
+
+    }
+
+    // async buscarUsuario(data: any) {
+    //     await knex('users').select()
+        
+        
+    // }
+
+    async updateUsuario(data: any) {
+
+    }
 
 }
