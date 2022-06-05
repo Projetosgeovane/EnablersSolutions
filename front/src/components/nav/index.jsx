75761982afa0a046ef8623703e88bd90a17481bd
Geovane Silva <geovanesilva.nar@hotmail.com>
2022-06-05 05:30:48 -0300

    Nav

--- front/src/components/nav/index.jsx
+++ front/src/components/nav/index.jsx
@@ -4,14 +4,10 @@ import { NavLink } from "react-router-dom";
 const Nav = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar navbar-dark bg-dark">
-            <NavLink to="/" className="navbar-brand">Administação</NavLink>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item active">
                         <NavLink className="nav-link" activeStyle={{ fontWeight: 'bold' }} to="/" >Clientes</NavLink>
-                    </li>
-                    <li className="nav-item">
-                        <NavLink className="nav-link" activeStyle={{ fontWeight: 'bold' }} to="/createclient">Cadastrar Cliente</NavLink>
                     </li>
                     <li className="nav-item">
                         <NavLink className="nav-link" activeStyle={{ fontWeight: 'bold' }} to="/user">Usuários</NavLink>
