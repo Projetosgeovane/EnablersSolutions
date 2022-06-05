5996f273bb8706f8937cd1a2aa328b25668e544f
Geovane Silva <geovanesilva.nar@hotmail.com>
2022-06-05 09:02:00 -0300

    Middlewares

--- backend/src/middlewares/handleError.ts
+++ backend/src/middlewares/handleError.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from "express";
 import { UnauthorizedError } from "express-jwt";
 import { ValidationError } from "express-validation";
 
-export default function (error: Error, req: Request, res: Response, next: NextFunction) {
+ function handleError (error: Error, req: Request, res: Response, next: NextFunction) {
   if (error instanceof ValidationError) {
     return res.status(error.statusCode).json(error);
   }
@@ -14,3 +14,5 @@ export default function (error: Error, req: Request, res: Response, next: NextFu
 
   return res.status(500).json(error);
 }
+
+export default handleError;
